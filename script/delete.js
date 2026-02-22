a20c45fb7c020723d7672142f9bb9d6a134aea47
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-02-22 18:59:36 +0600

    add job count

--- script/delete.js
+++ script/delete.js
@@ -1,7 +1,14 @@
 const deletes = document.querySelectorAll(".delete-btn");
 
+const jobId = document.getElementById("job-quantity");
+
 deletes.forEach((dt) => {
-  dt.addEventListener("click", {
-    
+  dt.addEventListener("click", function () {
+    const card = this.closest(".card");
+    card.remove();
+
+    const cardLeft = document.querySelectorAll(".card").length;
+    console.log(cardLeft);
+    jobId.innerHTML = cardLeft;
   })
 })
