75c47574ff3c6a1de1e6cc8367dafbe95eafb2e9
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-03-01 00:14:52 +0600

    if you will delete all card you see blank page

--- script/delete.js
+++ script/delete.js
@@ -13,16 +13,28 @@ deletes.forEach((dt) => {
       const hederInterviewCount = document.getElementById("header-interview");
       hederInterviewCount.innerText = interview;
       jobId.innerText = interview;
+      upJobId.innerText -= 1;
+      if(interview === 0) {
+        blankPage.classList.remove("hidden");
+      }
     }
     else if (card.classList.contains("rejected")) {
       const rejected = document.querySelectorAll(".rejected").length;
       const hederRejectedCount = document.getElementById("header-rejected");
       hederRejectedCount.innerText = rejected;
       jobId.innerText = rejected;
+      upJobId.innerText -= 1;
+      if(rejected === 0) {
+        blankPage.classList.remove("hidden");
+      }
     }
     else {
       const cardLeft = document.querySelectorAll(".card").length;
       jobId.innerHTML = cardLeft;
+      upJobId.innerHTML = cardLeft;
+      if(cardLeft === 0) {
+        blankPage.classList.remove("hidden");
+      }
     }
 
     upJobId.innerHTML = cardLeft;
